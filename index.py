36aebb5ff8377a111f5a076663b9cba9a4fc55e1
Janne Koponen <janne.koponen@wunder.io>
2018-11-30 09:22:30 +0200

    Add config and check access per slack token.

--- index.py
+++ index.py
@@ -1,21 +1,34 @@
 #!/usr/bin/python3
 import json
+import os
 import urllib.parse as urlparse
 from boto3 import client as boto3_client
-
 
 lambda_client = boto3_client('lambda', region_name="eu-west-1",)
 
 def respond(event, context):
     query = urlparse.parse_qs(event['body'])
-    lambda_client.invoke(
-            FunctionName="jannebot-dev-gentext",
-            InvocationType='Event',
-            Payload=json.dumps(query)
-        )
-    objRet =  {
-        'statusCode': 200,
-    }    
-    return objRet
+    token = os.environ['SLACK_TOKEN']
+    if (query['token'][0] != token):
+        data = {
+                "response_type": "ephemeral",
+                "text": "Oops, seems like you are not allowed here."
+                }
 
+        objRet =  {
+                'statusCode': 200,
+                'body': json.dumps(data),
+                }
+    else:
+        stage = os.environ['SERVERLESS_STAGE']
+        lambda_client.invoke(
+                FunctionName="jannebot-" + stage + "-gentext",
+                InvocationType='Event',
+                Payload=json.dumps(query)
+            )
+        objRet =  {
+            'statusCode': 200,
+        }
+
+    return objRet
 
